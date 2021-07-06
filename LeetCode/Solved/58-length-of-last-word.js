a5ca5b12861d4dce8776facb8bebba3414e8711a
Corey Noble <cnoble@evertz.com>
2021-07-06 14:26:35 -0400

    LeetCode #58: Length of Last Word

---
+++ LeetCode/Solved/58-length-of-last-word.js
@@ -0,0 +1,15 @@
+/**
+ * Determine the length of the last word in a string.
+ * @param {string} s
+ * @return {number}
+ */
+ var lengthOfLastWord = function(s) {   
+    /* Create an array of words using:
+       - .trimEnd() to remove trailing whitespace.
+       - .split(' ') to create the array of words, splitting at each space in the string. */
+    const words = s.trimEnd().split(' ');
+    // The last word is the last entry in the words array.
+    const lastWord = words[words.length-1];
+    
+    return lastWord.length;
+};
