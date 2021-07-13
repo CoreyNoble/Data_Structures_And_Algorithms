9a9c51b24f253356bd596687904ed3e6c0b7b00c
Corey Noble <cnoble@evertz.com>
2021-07-13 15:39:43 -0400

    LeetCode #100: Same Tree

---
+++ LeetCode/Solved/100-same-tree.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * Determines if two binary trees are identical.
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+
+ const isSameTree = (p, q) => {
+    // Both nodes are empty. Trees match.
+    if (!p && !q) {
+        return true 
+    // One node is present, the other node isn't. Trees don't match.
+    } else if (!p || !q) {
+        return false
+    // Values don't match. Trees don't match.
+    } else if (p.val !== q.val) {
+       return false 
+    // No case is met.
+    } else {
+        // Recurse down both sides of the tree.
+        return isSameTree(p.left, q.left) && 
+               isSameTree(p.right, q.right)
+    }
+}
